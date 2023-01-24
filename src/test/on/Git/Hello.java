6036e579516540a7bba9f36dacc5edaca2f8bf14
schowdh1 <soumitramc01@gmail.com>
2023-01-24 21:57:04 +0530

    Update Hello.java

--- src/test/on/Git/Hello.java
+++ src/test/on/Git/Hello.java
@@ -1,12 +1,38 @@
 package test.on.Git;
 
-public class Hello {
-
+import java.util.*;
+public class Main
+{
 	public static void main(String[] args) {
-		
-		
-		System.out.println("Hello!!!!!!!!!!!!!");
-
+	 	Scanner sc=new Scanner(System.in);
+		int max=sc.nextInt();
+		System.out.println("*");
+	 for (int i = 1; i <= max; i++)
+        {
+            System.out.print("*");
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println("*");
+        }
+        for (int i = max-1; i >= 1; i--)
+        {
+             System.out.print("*");
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println("*");
+        }
+        	System.out.println("*");
 	}
-
 }
